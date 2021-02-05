3099e8b71738ed8cefb68a8ddde12406ffd789bc
Matthew Rich <matthew.rich@mrichinfinite.com>
2021-02-05 16:14:33 -0600

    New Ruby code

--- scratch/scratch3/scratch3.rb
+++ scratch/scratch3/scratch3.rb
@@ -46,6 +46,52 @@ boolean = true # Boolean
 boolean2 = false # Boolean
 none = nil # nil (means no value)
 
+# Strings in Ruby
+puts "mrichinfinite"
+# Quotation marks within a string
+puts "\"I like cheese.\""
+# New line within a string
+puts "I love to\ndrink milk"
+# Using a variable
+phrase = "Let's have fun with Ruby"
+puts phrase
+
+# String methods:
+
+# Change everything to uppercase and then to lowercase
+puts "Ruby programming".upcase()
+puts "Ruby programming".downcase()
+puts phrase.upcase()
+puts phrase.downcase()
+# Remove whitespace from a string
+phrase2 = "     All this whitespace needs to go     "
+puts phrase2.strip()
+# Display how many characters are inside of a string
+puts phrase.length()
+puts phrase2.length()
+# Check to see if something is inside of a string
+puts phrase.include? "Ruby"
+puts phrase.include? "wow"
+puts phrase2.include? "whitespace"
+puts phrase2.include? "no way"
+# Check to see which character is at which index position in a string
+puts phrase[0]
+puts phrase2[11]
+puts phrase[2]
+puts phrase2[10]
+# Print a range of characters
+puts phrase[0,3]
+puts phrase[11,16]
+puts phrase2[6,9]
+puts phrase2[7,10]
+# Check to see at what index position a character resides
+puts phrase.index("h")
+puts phrase2.index("w")
+# Check to see at what index position a string of characters begins
+puts phrase.index("Ruby")
+puts phrase2.index("whitespace")
+
+# Google more string methods in Ruby!
 
 
 
