a61c01cf9ec6b9305f7fbe6eb04f1d45c30c3dd0
Matthew Rich <matthew.rich@mrichinfinite.com>
2021-03-23 16:30:19 -0500

    Add case expressions lesson

--- scratch/scratch3/scratch3.rb
+++ scratch/scratch3/scratch3.rb
@@ -379,3 +379,33 @@ elsif op == "/"
 else
     puts "Invalid operator, please enter a valid operator."
 end
+
+# Case expressions. We can use these in lieu of if statements in certain situations
+def get_day_name(day)
+    day_name = ""
+
+    case day
+    when "mon"
+        day_name = "Monday"
+    when "tue"
+        day_name = "Tuesday"
+    when "wed"
+        day_name = "Wednesday"
+    when "thu"
+        day_name = "Thursday"
+    when "fri"
+        day_name = "Friday"
+    when "sat"
+        day_name = "Saturday"
+    when "sun"
+        day_name = "Sunday"
+    else
+        day_name = "Invalid abbreviation"
+    end
+    return day_name
+end
+
+puts get_day_name("thu")
+puts get_day_name("wed")
+puts get_day_name("sun")
+puts get_day_name("cat")
