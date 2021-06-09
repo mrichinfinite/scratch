3338a9c84c24f2ea513358cf80f17a1251295b56
Matthew Rich <matthew.rich@mrichinfinite.com>
2021-06-09 16:48:09 -0500

    Add simple calculator script

---
+++ arithmeticcalculator/calculators.py
@@ -0,0 +1,13 @@
+from arithmetic import Arithmetic
+
+add = Arithmetic.addition(5, 6)
+print(add)
+
+subtract = Arithmetic.subtraction(6, 5)
+print(subtract)
+
+multiply = Arithmetic.multiplication(5, 6)
+print(multiply)
+
+divide = Arithmetic.division(100, 10)
+print(divide)
